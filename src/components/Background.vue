2172cd4e7004185c2f999e077e865beb227e91a8
Vinicius de Moraes Nascimento <38895221+Avxy@users.noreply.github.com>
2021-05-21 00:33:47 +0100

    Resumo

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,28 +3,26 @@
         BACKGROUND
         <q-separator />
         <div class="col-auto q-pt-xl q-pa-xl texting text-h4 text-justify">
-            Há um Aeon atrás, em uma terra que acabou se tornando um mito,
-             um calmo jovem se sentou enquanto fantasiava sobre os seus sonhos
-              ao lado de um rio. E foi de repente quando uma pequena tartaruga emergiu,
-               saindo da água orgulhosa pelo seu talento, sentou-se ao lado do jovem apenas
-                para compartilhar uma gentil contemplação pelo todo.
+            Era uma vez, talvez até um aeon atrás, em uma terra que agora se tornou um mito, 
+            um jovem sentou-se em profunda meditação ao lado de um grande rio.
+            De repente, uma pequena tartaruga emergiu do grande rio amarelo, e
+            saindo orgulhosamente da água, sentou-se ao lado dele para compartilhar sua gentil contemplação.
         </div>
         <div class="col-auto q-pt-xl q-pa-xl texting text-h4 text-justify">
-            E demorou um pouco até que finalmente o nosso jovem, que estava evidente a sua
-             grande e universal epifania, percebeu a presença da tartaruga e decidiu exclamar
-              a ela com um grande e maravilhoso suspiro de satisfação:
+            Logo o nosso jovem, que estava em uma grande epifania universal, 
+            percebeu a presença da tartaruga e decidiu exclamar com um maravilhoso suspiro de satisfação:
         </div>
         <div class="q-pt-xl q-pa-xl texting text-h4 text-justify">
             <i>Ah, pequeninha! Do que se trata tudo isto...?"</i>
         </div>
         <div class="q-pt-xl q-pa-xl texting text-h4 text-justify">
-            E para o seu espanto, a tartaruga volteou em um círculo quase perfeito,
-             com um jeitinho meio despreocupada, em seguida deu as costas ao jovem e
-              continuou a se bronzear preguiçosamente e em silêncio. O jovem observou
-               atentamente aquele comportamento, principalmente para as costas do pequeno
-                animal, aos padrões intricados e entrelaçados das placas de suas costas que 
-                aos poucos secava ao sol que a primavera trazia. Enquanto ele observava, mais 
-                sentiu algo a acontecer consigo.
+            E para o seu espanto, a tartaruga o envolveu em um círculo quase perfeito,
+            com um jeitinho meio despreocupada, em seguida deu as costas ao jovem e
+            continuou a se bronzear preguiçosamente e em silêncio. 
+            O jovem observou atentamente aquele comportamento, principalmente para as costas do pequeno
+            animal, aos padrões intricados e entrelaçados das placas de suas costas que 
+            aos poucos secava ao sol que a primavera trazia. Enquanto ele observava, mais 
+            sentiu algo a acontecer consigo.
         </div>
         <div class="q-pt-xl q-pa-xl texting text-h4 text-justify">
             Quanto mais ele observava a tartaruga, mais ele conseguia entender a natureza de
